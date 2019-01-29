453971772e50ebc7fabd9f217df30d20e2313307
Student <example@example.com>
2019-01-28 20:48:51 -0800

    calulator

---
+++ js/bmiscript.js
@@ -0,0 +1,8 @@
+var bmi = function(weight, height) {
+  return ((weight/(height^2))X703)
+};
+
+var weight = parseInt(prompt("Enter your weight:"));
+var height = parseInt(prompt("Enter your height:"));
+var bmiResult = bmi(weight, heigh);
+alert(bmiResult);
